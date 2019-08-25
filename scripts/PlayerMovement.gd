5fba5dbd96b329973ac791a0ff27b7cc4efe41bb
taylus <brandon@legacy.codes>
2019-08-25 16:29:09 -0500

    Make player face movement direction

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
 
 const walk_speed = 50;
+onready var sprite: Sprite = get_node("Sprite")
 
 func _physics_process(delta: float) -> void:
-    if Input.is_action_pressed("ui_left"):
-        move_and_slide(Vector2(-walk_speed, 0), Vector2.UP)
-    elif Input.is_action_pressed("ui_right"):
-        move_and_slide(Vector2(walk_speed, 0), Vector2.UP)
+	if Input.is_action_pressed("ui_left"):
+		move_and_slide(Vector2(-walk_speed, 0), Vector2.UP)
+		sprite.flip_h = false
+	elif Input.is_action_pressed("ui_right"):
+		move_and_slide(Vector2(walk_speed, 0), Vector2.UP)
+		sprite.flip_h = true
