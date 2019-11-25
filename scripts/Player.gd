20048b67851ad34562e7b8c692232c2e9e776393
taylus <brandon@legacy.codes>
2019-11-25 14:12:49 -0600

    come back to this later

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,13 @@
 extends Sprite
 
+onready var anim_timer = get_node("Animation Timer")
+
 var frames = [
 	preload("res://gfx/slime_idle_1.png"),
-	preload("res://gfx/slime_idle_2.png")
+	preload("res://gfx/slime_idle_2.png"),
 ]
+
+var on_ladder = preload("res://gfx/slime_on_ladder.png")
 
 func _on_Animation_Timer_timeout() -> void:
 	texture = frames[1] if texture == frames[0] else frames[0]
@@ -11,3 +15,9 @@ func _on_Animation_Timer_timeout() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+		
+	if get_parent().on_ladder:
+		anim_timer.paused = true
+		texture = on_ladder
+	else:
+		anim_timer.paused = false
