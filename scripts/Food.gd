cc08e98dbaba4f45122fae1fc892a402b5aefa46
taylus <brandon@legacy.codes>
2019-11-26 17:10:39 -0600

    Make food a reusable prefab/scene

--- scripts/Food.gd
+++ scripts/Food.gd
@@ -2,16 +2,21 @@ extends KinematicBody2D
 class_name Food
 
 export var tasty: bool
+export var texture: Texture
+export var score: int
 
-# TODO: have the player look up if food is above him,
-# and look up + smile if the food is tasty -- 
-# will make catching food right off the conveyor look cute
+func _ready() -> void:
+	if texture: get_node("Sprite").texture = texture
 
 func _physics_process(delta: float) -> void:
 	_fall_with_gravity()
 	
 func _fall_with_gravity(gravity: Vector2 = Vector2(0, 1)):
 	move_and_collide(gravity)
+
+# TODO: have the player look up if food is above him,
+# and look up + smile if the food is tasty -- 
+# will make catching food right off the conveyor look cute
 
 func _on_PlayerHitDetectionArea_area_entered(area: Area2D):
 	print("Food _on_body_entered w/ %s \"%s\"" % [str(area), area.name])
